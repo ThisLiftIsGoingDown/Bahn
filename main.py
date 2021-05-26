e620e2711c0338631da8e0e702a8df0127120d23
HELLO64t <david.bartsch@bluewin.ch>
2021-05-26 10:32:23 +0200

    created gui.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 import json
 URL = "https://fahrplan.search.ch/api/stationboard.json?"
 inf = {'stop':'Einsiedeln',
-        'limit': '2'}
+        'limit': '1'}
 r = requests.get(url = URL, params= inf)
 jsono = r.text
 d = json.loads(jsono)
-print(d["connections"][1]["time"])
+print(d["connections"][0]["terminal"]['name'])
